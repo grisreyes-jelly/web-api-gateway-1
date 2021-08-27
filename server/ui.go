f6587ce37fa95771ee6dbe3f781ba9fa6f373b34
grisreyes-jelly <grisell.reyes@jellyfish.com>
2021-08-27 17:00:34 -0500

    Añadiendo clientsecret para pruebas facebook

--- server/ui.go
+++ server/ui.go
@@ -310,7 +310,7 @@ func saveServiceHandler(w http.ResponseWriter, r *http.Request) *appError {
 	}
 
 	u.ClientID(r.FormValue("22446720946-fl25pr7jht8om683sopquhoao6skr67n.apps.googleusercontent.com"))
-	u.ClientSecret(r.FormValue("ClientSecret"))
+	u.ClientSecret(r.FormValue("vq7MGXfORSR_rQwhF0QdxmWJ"))
 
 	engineName := r.FormValue("Engine")
 	if engineName != "" {
